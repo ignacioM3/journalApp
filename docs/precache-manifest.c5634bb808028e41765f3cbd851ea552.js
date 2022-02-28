20ed156d3e61e3933272233a2ceb8fc95f477302
ignacioM3 <ignacio.marquez159@gmail.com>
2022-02-28 17:07:06 -0300

    sube docs

---
+++ docs/precache-manifest.c5634bb808028e41765f3cbd851ea552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9c007980607e2e5288aab9e0500416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "235939ee1696fa80a489",
+    "url": "/static/css/main.ac05a3bb.chunk.css"
+  },
+  {
+    "revision": "2b3a5dc1d5b662abfc06",
+    "url": "/static/js/2.a24694bf.chunk.js"
+  },
+  {
+    "revision": "e106df0353ed720b5a4c294de98cc088",
+    "url": "/static/js/2.a24694bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235939ee1696fa80a489",
+    "url": "/static/js/main.81bded31.chunk.js"
+  },
+  {
+    "revision": "1f9b80c10cb2174ac7b3",
+    "url": "/static/js/runtime-main.8c0f0de7.js"
+  }
+]);
